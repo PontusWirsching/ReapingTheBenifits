129daf83b655183533a9b03031bc94d97494d06a
Pontus <flash13records@gmail.com>
2014-08-24 20:25:36 +0200

    Added a task list!
    Signed-off-by: Pontus <flash13records@gmail.com>

--- src/com/game/main/Start.java
+++ src/com/game/main/Start.java
@@ -7,6 +7,31 @@ import com.lss.flasher.StateHandler.StateHandler;
 
 public class Start extends LEngine {
 	
+	
+	/*
+	 * Here is everything that i think is missing from the game. The things with arrows is easier
+	 * for you i think. Ask me or gavin if you have any questions!
+	 */
+	
+	//TODO - Change Loading and Saving dir to %appdata% lss folder
+	//TODO - Add more NPC's - Talk to GAVIN                                       <-----------------
+	//TODO - Add more tiles and design the map                                    <-----------------
+	//TODO - Add barricades around the map like road blocks and trees etc.        <-----------------
+	//TODO - Make the kill meter count kills.
+	//TODO - Add a menu                                 					      <-----------------
+	//TODO - Add a game over menu                             			          <-----------------
+	//TODO - Add looting money from different places like Bins and Under benches.
+	//TODO - Make so you get a random amount of money from NPC's.
+	//TODO - Add a shop with upgrades.              	                          <-----------------
+	//TODO - Fix the layered rendering.
+	
+	
+	
+	
+	
+	
+	
+	
 	public Start(int width, int height, String frameName) {
 		super(width, height, frameName);
 
